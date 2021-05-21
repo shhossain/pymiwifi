cf07e8f5931254ca574177e4a57680ca0edd0c72
shhossain <80335059+shhossain@users.noreply.github.com>
2021-05-21 18:02:03 +0600

    All Error Handled

--- pymiwifi/api.py
+++ pymiwifi/api.py
@@ -1,24 +1,23 @@
-import hashlib
-import random
-import time
-import uuid
-
-import requests
+from hashlib import sha1 as encrypt
+from random import random
+from time import time
+from uuid import getnode
+from requests import post,get
 
 PUBLIC_KEY = "a2ffa5c9be07488bbb04a3a47d3c5f6a"
 
 
 def sha1(x: str):
-    return hashlib.sha1(x.encode()).hexdigest()
+    return encrypt(x.encode()).hexdigest()
 
 
 def get_mac_address():
-    as_hex = f"{uuid.getnode():012x}"
+    as_hex = f"{getnode():012x}"
     return ":".join(as_hex[i : i + 2] for i in range(0, 12, 2))
 
 
 def generate_nonce(miwifi_type=0):
-    return f"{miwifi_type}_{get_mac_address()}_{int(time.time())}_{int(random.random() * 1000)}"
+    return f"{miwifi_type}_{get_mac_address()}_{int(time())}_{int(random() * 1000)}"
 
 
 def generate_password_hash(nonce, password):
@@ -26,9 +25,11 @@ def generate_password_hash(nonce, password):
 
 
 class MiWiFi:
-    def __init__(self, address="http://miwifi.com/", miwifi_type=0):
+    def __init__(self, address, miwifi_type=0):
         if address.endswith("/"):
             address = address[:-1]
+        if not "http" in address and not "https" in address:
+            address = "http://"+address
 
         self.address = address
         self.token = None
@@ -37,27 +38,46 @@ class MiWiFi:
     def login(self, password):
         nonce = generate_nonce(self.miwifi_type)
 
-        response = requests.post(
+        try:
+            response = post(
             f"{self.address}/cgi-bin/luci/api/xqsystem/login",
             data={
                 "username": "admin",
                 "logtype": "2",
-                "password": generate_password_hash(nonce, password),
+                "password": generate_password_hash(f"{nonce}", password),
                 "nonce": nonce,
-            },
-        )
+            },)
+        except:
+            error_msg = f"Check if {self.address} is your router login page."
+            raise ValueError(error_msg)
+
+        
         if response.status_code == 200:
-            self.token = response.json()["token"]
+            text = response.text[0:10]
+            if text.startswith("{"):
+                js = response.json()
+                code = js['code']
+                if code==0:
+                    self.token = js['token']
+                else:
+                    msg = js['msg']
+                    if msg=="not auth":
+                        msg = "You Don't have authority to login or you've entered an incorrect password."
+
+                    raise ValueError(msg)
+            else:
+                error_msg = f"Check if {self.address} is your router login page."
+                raise ValueError(error_msg)
 
         return response
 
     def logout(self):
-        return requests.get(
+        return get(
             f"{self.address}/cgi-bin/luci/;stok={self.token}/web/logout"
         )
 
     def get_api_endpoint(self, endpoint):
-        return requests.get(
+        return get(
             f"{self.address}/cgi-bin/luci/;stok={self.token}/api/{endpoint}"
         ).json()
 
